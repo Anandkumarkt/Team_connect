99511a9429e0ff475fe2e47f2014c1acb9a5803e
Anandkumarkt <109223129+Anandkumarkt@users.noreply.github.com>
2023-11-08 16:19:04 +0530

    Update EmployeeService.java

--- src/main/java/com/example/teamconnect/service/EmployeeService.java
+++ src/main/java/com/example/teamconnect/service/EmployeeService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface EmployeeService {
-     ResponseEntity<ResponseClass> login(EmployeeDto employeeDto) throws LoginFailedException;
+     ResponseEntity<ResponseClass> login(EmployeeDto employeeDto);
 
 
     ResponseEntity<ResponseClass> registeration(EmployeeDto employeeDto,HttpServletRequest request) throws DataAlreadyExistsException;  // by default the access specifier is public abstract for interface

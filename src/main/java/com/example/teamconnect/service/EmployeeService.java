539554fc21629be3383e92ef7b2a103089d6bfd8
Anandkumarkt <109223129+Anandkumarkt@users.noreply.github.com>
2023-11-08 16:13:18 +0530

    Update EmployeeService.java

--- src/main/java/com/example/teamconnect/service/EmployeeService.java
+++ src/main/java/com/example/teamconnect/service/EmployeeService.java
@@ -13,7 +13,7 @@ public interface EmployeeService {
      ResponseEntity<ResponseClass> login(EmployeeDto employeeDto) throws LoginFailedException;
 
 
-    ResponseEntity<ResponseClass> registeration(EmployeeDto employeeDto,HttpServletRequest request) throws DataAlreadyExistsException, LoginFailedException;  // by default the access specifier is public abstract for interface
+    ResponseEntity<ResponseClass> registeration(EmployeeDto employeeDto,HttpServletRequest request) throws DataAlreadyExistsException;  // by default the access specifier is public abstract for interface
 
     ResponseEntity<ResponseClass> signUpMasterUser(EmployeeDto employeeDto, HttpServletRequest request) throws DataAlreadyExistsException;
 }
